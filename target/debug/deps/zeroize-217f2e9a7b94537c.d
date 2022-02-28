1136d964001b28adce668cc7cfdfdbecadab7dc2
jonbae <jonbae.mingun@gmail.com>
2022-02-27 21:53:08 -0600

    refresh gitignore

--- target/debug/deps/zeroize-217f2e9a7b94537c.d
+++
@@ -1,8 +0,0 @@
-/Users/jonathanbae/Documents/rusty_wordle/target/debug/deps/zeroize-217f2e9a7b94537c.rmeta: /Users/jonathanbae/.cargo/registry/src/github.com-1ecc6299db9ec823/zeroize-1.5.2/src/lib.rs /Users/jonathanbae/.cargo/registry/src/github.com-1ecc6299db9ec823/zeroize-1.5.2/src/x86.rs
-
-/Users/jonathanbae/Documents/rusty_wordle/target/debug/deps/libzeroize-217f2e9a7b94537c.rlib: /Users/jonathanbae/.cargo/registry/src/github.com-1ecc6299db9ec823/zeroize-1.5.2/src/lib.rs /Users/jonathanbae/.cargo/registry/src/github.com-1ecc6299db9ec823/zeroize-1.5.2/src/x86.rs
-
-/Users/jonathanbae/Documents/rusty_wordle/target/debug/deps/zeroize-217f2e9a7b94537c.d: /Users/jonathanbae/.cargo/registry/src/github.com-1ecc6299db9ec823/zeroize-1.5.2/src/lib.rs /Users/jonathanbae/.cargo/registry/src/github.com-1ecc6299db9ec823/zeroize-1.5.2/src/x86.rs
-
-/Users/jonathanbae/.cargo/registry/src/github.com-1ecc6299db9ec823/zeroize-1.5.2/src/lib.rs:
-/Users/jonathanbae/.cargo/registry/src/github.com-1ecc6299db9ec823/zeroize-1.5.2/src/x86.rs:
